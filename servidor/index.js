c682ffb5a154ff2e1becce3b05c6081be8743ba3
sepwizzz <felipeeeeeeeeeee1212@gmail.com>
2023-09-22 00:30:23 -0500

    first commit

--- servidor/index.js
+++ servidor/index.js
@@ -9,40 +9,6 @@ const cors =require("cors")
 
 
 // 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-  
-    next();
-  });
-  
-  app.get('/users', async (_req, res) => {
-    const url = 'https://randomuser.me/api/';
-  
-    try {
-      const response = await fetch(url);
-  
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      return res.json(result);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({error: 'An error occurred'});
-    }
-  });
-  
-  const port = 3000;
-  
-  app.listen(port, () =>
-    console.log(`Server running on http://localhost:${port}`),
-  );
-
-
-
-
 
 
 
@@ -61,17 +27,8 @@ const db =mysql.createConnection({
     database:"ad_hotel"
 })
 
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'});
-  });
-  
-  const PORT = 3000;
-  
-  app.listen(PORT, function () {
-    console.log(`CORS-enabled web server listening on port ${PORT}`);
-  });
 
-app.post("/create",(req,res)=>{
+app.post("/creat",(req,res)=>{
     const id_usuario = req.body.id_usuario;
     const usuario = req.body.id_usuario;
     const nombre = req.body.id_usuario;
@@ -79,7 +36,6 @@ app.post("/create",(req,res)=>{
     const correo  = req.body.id_usuario;
     const password = req.body.id_usuario;
     const id_tipo = req.body.id_usuario;
-    res.json({mgs:" This is CORS-enabled for all origins!"});
 
 
     db.query( "insert into  datos_usuario(id_usuario,usuario,nombre,apellido,correo,pasword,id_tipo) values (?,?,?,?,?,?,?)",[id_usuario,usuario,nombre,apellido,correo,password,
