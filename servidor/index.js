2139787da8fe39a351422ffc88447d8dd3425af3
sepwizzz <felipeeeeeeeeeee1212@gmail.com>
2023-09-22 02:00:25 -0500

    first commit

--- servidor/index.js
+++ servidor/index.js
@@ -1,19 +1,14 @@
-
-
+// nota iniciar prmero el servidor en la consola ( cd servidor  despues  node index.js)
+// despues  iniciar el front con (cb cliente  despues npm start)
 const express = require("express");
 
 const app=express();
 const mysql =require("mysql");
 
 const cors =require("cors")
-
-
 // 
 
-
-
 // 
-
 
 app.use(cors());
 
@@ -24,21 +19,22 @@ const db =mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
-    database:"ad_hotel"
+    database:"db_hotel"
 })
+// 
 
-
-app.post("/creat",(req,res)=>{
+// 
+app.post("/create",(req,res)=>{
     const id_usuario = req.body.id_usuario;
-    const usuario = req.body.id_usuario;
-    const nombre = req.body.id_usuario;
-    const apellido = req.body.id_usuario;
-    const correo  = req.body.id_usuario;
-    const password = req.body.id_usuario;
-    const id_tipo = req.body.id_usuario;
+    const usuario = req.body.usuario;
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const correo  = req.body.correo;
+    const password = req.body.password;
+    const id_tipo = req.body.id_tipo;
 
 
-    db.query( "insert into  datos_usuario(id_usuario,usuario,nombre,apellido,correo,pasword,id_tipo) values (?,?,?,?,?,?,?)",[id_usuario,usuario,nombre,apellido,correo,password,
+    db.query( "insert into  datos_usuario (id_usuario,usuario,nombre,apellido,correo,pasword,id_tipo) values (?,?,?,?,?,?,?)",[id_usuario,usuario,nombre,apellido,correo,password,
         id_tipo],(error,result)=>{
             if(error){
                 console.log(error);
@@ -46,6 +42,12 @@ app.post("/creat",(req,res)=>{
         });
     
 });
+// db.query( "insert into  datos_usuario (id_usuario,usuario,nombre,apellido,correo,pasword,id_tipo) values (?,?,?,?,?,?,?)",[1,"2","s","s","s","S",
+//     3],(error,result)=>{
+//         if(error){
+//             console.log(error);
+//         }else{res.send("registro exitoso")}
+//     });
 
 
 
